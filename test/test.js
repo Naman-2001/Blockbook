6f944a017ad0297c7ad495ae45a5ac0d08a21fd8
unknown <naman.aggarwal2001@gmail.com>
2021-01-02 22:17:47 +0530

    tipping function

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+const { assert } = require("chai");
+
 const Decentragram = artifacts.require("./Decentragram.sol");
 
 require("chai")
@@ -27,12 +29,48 @@ contract("Decentragram", ([deployer, author, tipper]) => {
   });
 
   describe("images", async () => {
-    let result;
+    let result, imageCount;
+    const hash = "abc123";
+    before(async () => {
+      result = await decentragram.uploadImage(hash, "Image description", {
+        from: author,
+      });
+      imageCount = await decentragram.imageCount();
+    });
 
     it("creates images", async () => {
-      result = await decentragram.uploadImage();
-      let image = await decentragram.images(1);
-      console.log(image);
+      //Success
+      assert.equal(imageCount, 1);
+      // console.log(result.logs[0].args);
+      const event = result.logs[0].args;
+      assert.equal(event.id.toNumber(), imageCount.toNumber(), "id is correct");
+      assert.equal(event.hash, hash, "Hash is correct");
+      assert.equal(
+        event.description,
+        "Image description",
+        "description is correct"
+      );
+      assert.equal(event.tipAmount, "0", "tip amount is correct");
+      assert.equal(event.author, author, "author is correct");
+
+      //Failures
+      await decentragram.uploadImage("", "Image decrip", { from: author })
+        .should.be.rejected;
+      await decentragram.uploadImage("Image decrip", "", { from: author })
+        .should.be.rejected;
+    });
+
+    it("lists images", async () => {
+      const image = await decentragram.images(imageCount);
+      assert.equal(image.id.toNumber(), imageCount.toNumber(), "id is correct");
+      assert.equal(image.hash, hash, "Hash is correct");
+      assert.equal(
+        image.description,
+        "Image description",
+        "description is correct"
+      );
+      assert.equal(image.tipAmount, "0", "tip amount is correct");
+      assert.equal(image.author, author, "author is correct");
     });
   });
 });
