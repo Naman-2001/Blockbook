52ebf5c56fdf1f6ee622682df79af69648182424
unknown <naman.aggarwal2001@gmail.com>
2021-01-03 19:45:07 +0530

    connected to metamask

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,52 @@
-import React, { Component } from 'react';
-import Web3 from 'web3';
-import Identicon from 'identicon.js';
-import './App.css';
-import Decentragram from '../abis/Decentragram.json'
-import Navbar from './Navbar'
-import Main from './Main'
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import Identicon from "identicon.js";
+import "./App.css";
+import Decentragram from "../abis/Decentragram.json";
+import Navbar from "./Navbar";
+import Main from "./Main";
 
+function App() {
+  const [account, setAccount] = useState("");
+  const [loading, setLoading] = useState(false);
 
-class App extends Component {
+  //Connect with metamask
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
+  async function loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("non ethereum browser detected.");
     }
   }
-
-  render() {
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-        { this.state.loading
-          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          : <Main
-            // Code...
-            />
-          }
-        }
-      </div>
-    );
+  async function loadBlockChaindata() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);
   }
+
+  useEffect(() => {
+    loadWeb3();
+    loadBlockChaindata();
+  }, []);
+
+  return (
+    <div>
+      <Navbar account={account} />
+      {loading ? (
+        <div id="loader" className="text-center mt-5">
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <Main
+        // Code...
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
