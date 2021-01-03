89bfcaecf4b7e47bb6c9abc13d924e3f90a9f482
unknown <naman.aggarwal2001@gmail.com>
2021-01-03 21:38:44 +0530

    ipfs connected and checked

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,21 @@ import Decentragram from "../abis/Decentragram.json";
 import Navbar from "./Navbar";
 import Main from "./Main";
 
+//Connect to ipfs
+const ipfsClient = require("ipfs-http-client");
+const ipfs = ipfsClient({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
+});
+
 function App() {
   const [account, setAccount] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [decentragram, setDecentragram] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [imageCount, setImageCount] = useState(0);
+  const [images, setImages] = useState([]);
+  const [buffer, setBuffer] = useState([]);
 
   //Connect with metamask
 
@@ -26,12 +38,61 @@ function App() {
     const web3 = window.web3;
     const accounts = await web3.eth.getAccounts();
     setAccount(accounts[0]);
+
+    const networkId = await web3.eth.net.getId();
+    const networkData = Decentragram.networks[networkId];
+    if (networkData) {
+      const decentragram = web3.eth.Contract(
+        Decentragram.abi,
+        networkData.address
+      );
+      setDecentragram(decentragram);
+
+      const imagesCount = await decentragram.methods.imageCount().call();
+      setImageCount(imagesCount);
+      setLoading(false);
+    } else {
+      window.alert("Decentagram contract not deploed to detected network");
+    }
   }
 
   useEffect(() => {
     loadWeb3();
     loadBlockChaindata();
   }, []);
+
+  const captureFile = (e) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+
+    reader.onloadend = () => {
+      setBuffer(Buffer(reader.result));
+      console.log(Buffer(reader.result));
+    };
+  };
+
+  const uploadImage = (desc) => {
+    console.log("Submitting files to ipfs");
+
+    ipfs.add(buffer, (error, result) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      console.log("Ipfs result", result);
+
+      // setLoading(true);
+      // decentragram.methods
+      //   .uploadImage(result[0].hash, desc)
+      //   .send({ from: account })
+      //   .on("transactionHash", (hash) => {
+      //     setLoading(false);
+      //     console.log(hash);
+      //   });
+    });
+  };
 
   return (
     <div>
@@ -41,9 +102,7 @@ function App() {
           <p>Loading...</p>
         </div>
       ) : (
-        <Main
-        // Code...
-        />
+        <Main captureFile={captureFile} uploadImage={uploadImage} />
       )}
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Identicon from "identicon.js";
 
-function Main() {
+function Main(props) {
+  const [description, setDesciption] = useState("");
+
+  const handleSubmit = (e) => {
+    // setDesciption
+    e.preventDefault();
+    props.uploadImage(description);
+  };
+
   return (
     <div className="container-fluid mt-5">
       <div className="row">
@@ -12,9 +20,31 @@ function Main() {
         >
           <div className="content mr-auto ml-auto">
             <p>&nbsp;</p>
-            <h1 className="d-4">Edit this file in App.js!</h1>
-
-            {/* Code ... */}
+            <form onSubmit={handleSubmit}>
+              <input
+                type="file"
+                accept=".jpg, .jpeg, .png, .bmp, .gif"
+                onChange={(e) => props.captureFile(e)}
+              />
+              <div className="form-group mr-sm-2">
+                <br></br>
+                <input
+                  id="imageDescription"
+                  type="text"
+                  value={description}
+                  onChange={(e) => setDesciption(e.target.value)}
+                  className="form-control"
+                  placeholder="Image description..."
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary btn-block btn-lg"
+              >
+                Upload!
+              </button>
+            </form>
 
             <p>&nbsp;</p>
 
